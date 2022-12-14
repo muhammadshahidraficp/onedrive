42968c89d4fc2fb98109c2f23d3ad25c6b71de4c
Muhammad Shahid Rafi C P - MacBook Pro <muhammadshahidraficp@gmail.com>
2022-12-14 16:27:00 +0400

    FIX:upload and write to db

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import AddIcon from '@material-ui/icons/Add';
 import "./File.css";
 import Modal from "@material-ui/core/Modal";
 import { makeStyles } from '@material-ui/core/styles';
+import fire from '../../firebase';
+import { ref as sRef, uploadBytes,getDownloadURL } from "firebase/storage";
+import uuid from 'react-uuid';
+import {getDatabase,set, ref as dRef} from "firebase/database";
+import {setDoc,doc} from "firebase/firestore";
 
 function getModalStyle() {
     return {
@@ -24,20 +29,36 @@ const useStyles = makeStyles((theme) => ({
 
 
 const File = () => {
+    
     const classes = useStyles();
     const [open, setOpen] = useState(false);
     const [file, setFile] = useState(null);
     const [uploading, setUploading] = useState(false);
     const [modalStyle] = useState(getModalStyle);
-
+    const database = getDatabase();
     const handleOpen = () => { setOpen(true); };
     const handleClose = () => { setOpen(false); };
 
-    const handleChange = (e) => { }
-
-    const handleUpload = () => {
-        setUploading(true)
+    const handleChange = (e) => { 
+        setFile(e.target.files[0])
     }
+
+    const handleUpload = async() => {
+        setUploading(true);
+        const storageRef = sRef(fire.storage, `images/${uuid()}`);
+        const uploadResult = await uploadBytes(storageRef, file);
+        const link = await getDownloadURL(uploadResult.ref);
+        await setDoc(doc(fire.firestore,"users",uuid()),{
+            link
+        });
+        // console.log(link);
+        // await set(dRef(database,"users"),{link})
+        setUploading(false);
+    }
+
+    useEffect(() => {
+
+    }, []);
 
     return (<div className="file">
         <div className="file_container" onClick={handleOpen}>
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage,ref } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,5 +17,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const firestore = getFirestore(app);
 
-export default app;
+const fire = {app,storage,firestore};
+
+export default fire;
